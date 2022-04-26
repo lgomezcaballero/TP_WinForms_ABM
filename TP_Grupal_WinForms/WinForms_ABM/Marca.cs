2e2689b55371226e7a7ec584de0aa550c9538c6f
Tobias Iaconis adasme <tobias_iaconis99@hotmail.com>
2022-04-26 20:24:55 -0300

    Agregue la clase Marca y la clase Categoria

---
+++ TP_Grupal_WinForms/WinForms_ABM/Marca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms_ABM
+{
+    internal class Marca
+    {
+        public int id { get; set; }
+
+        public string Descripcion { get; set; }
+
+    }
+}
