1b29fd4caadafd48395080f1438a39c2d7810739
lgomezcaballero <leandrogomez343@gmail.com>
2022-04-29 21:39:58 -0300

    Se agregan clases MarcaNegocio y CategoriaNegocio

---
+++ Negocio/CategoriaNegocio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+
+namespace Negocio
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datosCategoria = new AccesoDatos();
+            try
+            {
+                datosCategoria.setConsulta("Select Id, Descripcion From CATEGORIAS");
+                datosCategoria.ejecutarLectura();
+
+                while (datosCategoria.Lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.ID = (int)datosCategoria.Lector["Id"];
+                    aux.Descripcion = (string)datosCategoria.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datosCategoria.cerrarConexion();
+            }
+        }
+    }
+}
---
+++ Negocio/MarcaNegocio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Negocio
+{
+    public class MarcaNegocio
+    {
+        public List<Marca> listar()
+        {
+            List<Marca> lista = new List<Marca>();
+            AccesoDatos datosMarca = new AccesoDatos();
+            try
+            {
+                datosMarca.setConsulta("Select Id, Descripcion From MARCAS");
+                datosMarca.ejecutarLectura();
+
+                while (datosMarca.Lector.Read())
+                {
+                    Marca aux = new Marca();
+                    aux.ID = (int)datosMarca.Lector["Id"];
+                    aux.Descripcion = (string)datosMarca.Lector["Descripcion"];
+
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                datosMarca.cerrarConexion();
+            }
+        }
+    }
+}
